b6b0c758a7776c9ae6708fbb9acdb8ef637fc6a9
Dorian Young <DorianYoung@Ymail.com>
2020-01-15 12:24:58 -0800

    Finished html structure and css. Need to finish js.

--- Assets/passwordGenerator.js
+++ Assets/passwordGenerator.js
@@ -1,12 +1,9 @@
+// Form Output 
 
-//Form Output
-function myFunction() {
-    var userInput = document.getElementById("formInput");
-    if (!userInput.checkValidity()) {
-      document.getElementById("formOutput").innerHTML = userInput.validationMessage;
-    } else {
-      document.getElementById("formOutput").innerHTML = "Your Password is: ";
-    } 
+function showInput() {
+  if (!userInput.checkValidity()) {
+    document.getElementById("display").innerHTML = userInput.validationMessage;
+  } else {
+    document.getElementById("display").innerHTML = "Your Password is: " + document.getElementById("userInput").value;
   } 
-
- 
+  }
